43bb8e05b7d694707e8dd405ab4ffe42355c1f72
phreakyGeek <mridulinlko@gmail.com>
2023-05-01 00:53:57 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://memories-mridul.herokuapp.com'})
+const API = axios.create({ baseURL: 'https://jealous-duck-fashion.cyclic.app'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
